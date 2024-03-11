309051824bdbc3f701877e0efa08aeea7b398dc9
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-03-11 12:48:41 -0500

    resolving incorrect file paths - testing DB connection

--- capstone-team-1/src/App.jsx
+++ capstone-team-1/src/App.jsx
@@ -1,40 +1,40 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import JobList from './components/Main/JobList'
-import data_jobs from './data/data.json'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+// import JobList from './components/Main/JobList'
+import data_jobs from './data/joblist.json';
 
 function App() {
-  const [jobs, setJobs] = useState([])
-  const [filters, setFilters] = useState([])
+  const [jobs, setJobs] = useState([]);
+  const [filters, setFilters] = useState([]);
 
-  useEffect(() => setJobs(data_jobs))
+  useEffect(() => setJobs(data_jobs));
 
   // filter function
   const filterFunc = ({ role, level, tools, languages }) => {
     if (filters.length === 0) {
-      return true
+      return true;
     }
 
-    const tags = [role, level]
+    const tags = [role, level];
 
-    return tags.some((tag) => filters.includes(tag))
-  }
+    return tags.some((tag) => filters.includes(tag));
+  };
 
-  const filteredJobs = jobs.filter(filterFunc)
+  const filteredJobs = jobs.filter(filterFunc);
 
   return (
-    <div className='w-full min-h-screen'>
+    <div className="w-full min-h-screen">
       <Header />
-      <div className='max-w-7xl mx-auto'>{/* use filter function here */}</div>
+      <div className="max-w-7xl mx-auto">{/* use filter function here */}</div>
 
       {/* joblist component here */}
       <JobList filteredJobs={filteredJobs} />
       {/* Footer component here */}
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
