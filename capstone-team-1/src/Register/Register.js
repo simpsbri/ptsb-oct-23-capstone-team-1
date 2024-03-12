f132cb020b9eb36b632ae60f19d4196ba200072d
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-03-11 21:11:39 -0500

    adding in states

--- capstone-team-1/src/Register/Register.js
+++ capstone-team-1/src/Register/Register.js
@@ -1,1 +1,30 @@
 import { useRef, useState, useEffect } from 'react';
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/register';
+
+import React from 'react';
+
+const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState('');
+  const [validPwd, setValidPwd] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  return <div>Register</div>;
+};
+
+export default Register;
