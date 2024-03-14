aeadaa93d52bdf64a05826b0eac9b2fe6e247e61
Mi-Stap <mikeystaples@gmail.com>
2024-03-14 19:04:29 -0400

    Extended User model with new fields (role, postedAt, status, languages) for frontend compatibility. - Resolved issues with pushing email notification feature using nodemailer.

--- capstone-team-1/server/models/userModel.js
+++ capstone-team-1/server/models/userModel.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userModel = mongoose.Schema(
   {
@@ -13,6 +13,10 @@ const userModel = mongoose.Schema(
       type: Boolean,
       required: true,
       default: false,
+      role: { type: String, required: true },
+      postedAt: { type: Date, required: true },
+      status: { type: String, required: true },
+      languages: { type: [String], required: true },
     },
   },
 
@@ -25,14 +29,14 @@ userModel.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userModel.pre('save', async function (next) {
-  if (!this.isModified('password')) {
+userModel.pre("save", async function (next) {
+  if (!this.isModified("password")) {
     return next();
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-const User = mongoose.model('User', userModel);
+const User = mongoose.model("User", userModel);
 
 module.exports = User;
--- capstone-team-1/src/components/User/UserCard.jsx
+++ capstone-team-1/src/components/User/UserCard.jsx
@@ -1,29 +1,29 @@
 const UserCard = ({ user, handleEditClick }) => {
-  const { name, email, isAdmin, role, postedAt, status, languages } = user
+  const { name, email, isAdmin, role, postedAt, status, languages } = user;
 
   return (
-    <div className='flex flex-col justify-between bg-white shadow-md my-5 mx-10 p-6 rounded-md border-teal-500 border-solid sm:flex-row'>
-      <div className='flex-flex-col-justify-between ml-4'>
+    <div className="flex flex-col justify-between bg-white shadow-md my-5 mx-10 p-6 rounded-md border-teal-500 border-solid sm:flex-row">
+      <div className="flex-flex-col-justify-between ml-4">
         {/* user info */}
-        <h1 className='py-2 text-primary_dark_cyan text-lg'>
+        <h1 className="py-2 text-primary_dark_cyan text-lg">
           {name}
           {isAdmin && (
-            <span className='bg-primary_dark_cyan rounded-full px-3 text-base text-white ml-4'>
+            <span className="bg-primary_dark_cyan rounded-full px-3 text-base text-white ml-4">
               Admin
             </span>
           )}
         </h1>
-        <p className='text-dark_gray_cyan text-base'>{email}</p>
+        <p className="text-dark_gray_cyan text-base">{email}</p>
         {/* job info */}
-        <p className='flex items-center gap-2 text-dark_gray_cyan text-base pr-6'>
+        <p className="flex items-center gap-2 text-dark_gray_cyan text-base pr-6">
           {role} - {status} - {postedAt}
         </p>
         {/* languages */}
-        <div className='flex items-center gap-2'>
+        <div className="flex items-center gap-2">
           {languages.map((language, index) => (
             <span
               key={`language-${index}`}
-              className='bg-light_grayish_cyan_filter_pill font-bold p-2 rounded mr-4'
+              className="bg-light_grayish_cyan_filter_pill font-bold p-2 rounded mr-4"
             >
               {language}
             </span>
@@ -31,16 +31,16 @@ const UserCard = ({ user, handleEditClick }) => {
         </div>
       </div>
       {/* Edit user button */}
-      <div className='flex items-center mt-4 mx-4 pt-4 sm:ml-auto'>
+      <div className="flex items-center mt-4 mx-4 pt-4 sm:ml-auto">
         <button
-          className='bg-primary_dark_cyan text-white font-bold p-2 rounded'
+          className="bg-primary_dark_cyan text-white font-bold p-2 rounded"
           onClick={() => handleEditClick(user)}
         >
           Edit User
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
