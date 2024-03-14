2822315227948dbff357ef146ceb0d8be6e4c2b2
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-03-14 08:09:48 -0500

    added in confirm password input

--- capstone-team-1/src/Register/Register.js
+++ capstone-team-1/src/Register/Register.js
@@ -133,6 +133,33 @@ const Register = () => {
         <span aria-label="dollar sign">$</span>
         <span aria-label="percent">%</span>
       </p>
+
+      <label htmlFor="confirm_pwd">
+        Confirm Password:
+        <span className={validMatch && matchPwd ? 'valid' : 'hide'}>
+          <FontAwesomeIcon icon={faTimes} />
+        </span>
+        <span className={validMatch || !matchPwd ? 'hide' : 'invalid'}>
+          <FontAwesomeIcon icon={faTimes} />
+        </span>
+      </label>
+      <input
+        type="password"
+        id="confirm_pwd"
+        onChange={(e) => setMatchPwd(e.target.value)}
+        required
+        aria-invalid={validMatch ? 'false' : 'true'}
+        aria-describedby="confirmnote"
+        onFocus={() => setMatchFocus(true)}
+        onBlur={() => setMatchFocus(false)}
+      />
+      <p
+        id="confirmnote"
+        className={matchFocus && !validMatch ? 'instructions' : 'offscreen'}
+      >
+        <FontAwesomeIcon icon={faInfoCirlce} />
+        Must match the first password input field.
+      </p>
     </form>
   </section>;
 };
