de403fe944c2495cc473fd667c29ca04c4f64b9d
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-03-13 19:00:45 -0500

    added in input for username

--- capstone-team-1/src/Register/Register.js
+++ capstone-team-1/src/Register/Register.js
@@ -55,7 +55,43 @@ const Register = () => {
     setErrMsg('');
   }, [user, pwd, matchPwd]);
 
-  return <section>Register</section>;
+  return;
+  <section>
+    <p
+      ref={errRef}
+      className={errMsg ? 'errmsg' : 'offscreen'}
+      aria-live="assertive"
+    >
+      {errMsg}
+    </p>
+    <h1>Register</h1>
+    <form>
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        ref={userRef}
+        autoComplete="off"
+        onChange={(e) => setUser(e.target.value)}
+        required
+        aria-invalid={validName ? 'false' : 'true'}
+        aria-describedby="uidnote"
+        onFocus={() => setUserFocus(true)}
+        onBlur={() => setUserFocus(false)}
+      />
+      <p
+        id="uidnote"
+        className={
+          userFocus && user && !validName ? 'instructions' : 'offscreen'
+        }
+      >
+        <FontAwesomeIcon icon={faInfoCircle} />
+        4 to 24 characters. <br />
+        Must begin with a letter. <br />
+        Letters, numbers, underscores, hyphens allowed.
+      </p>
+    </form>
+  </section>;
 };
 
 export default Register;
