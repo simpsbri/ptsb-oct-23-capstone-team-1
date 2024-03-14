a045bcc695a7a4da223cb33ce75cc86f84d1e6ec
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-03-13 20:35:59 -0500

    added password input and instructions

--- capstone-team-1/src/Register/Register.js
+++ capstone-team-1/src/Register/Register.js
@@ -108,6 +108,31 @@ const Register = () => {
           <FontAwesomeIcon icon={faTimes} />
         </span>
       </label>
+      <input
+        type="password"
+        id="password"
+        onChange={(e) => setPwd(e.target.value)}
+        required
+        aria-invalid={validPwd ? 'false' : 'true'}
+        aria-describedby="pwdnote"
+        onFocus={() => setPwdFocus(true)}
+        onBlur={() => setPwdFocus(false)}
+      />
+      <p
+        id="pwdnote"
+        className={pwdFocus && !validPwd ? 'instructions' : 'offscreen'}
+      >
+        <FontAwesomeIcon icon={fainfoCircle} />
+        8 to 24 characters. <br />
+        Must include uppercase and lowercase letters, a number and a special
+        character.
+        <br />
+        Allowed special characters: <span aria-label="exclamation mark">!</span>
+        <span aria-label="at symbol">@</span>{' '}
+        <span aria-label="hashtag">#</span>
+        <span aria-label="dollar sign">$</span>
+        <span aria-label="percent">%</span>
+      </p>
     </form>
   </section>;
 };
