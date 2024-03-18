77b8af75925917445b61ebe709120797a3375a6d
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-03-18 12:14:53 -0500

    adding in material UI container for uniform margins

--- capstone-team-1/src/components/MainLayout.jsx
+++ capstone-team-1/src/components/MainLayout.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import Header from './Header/Header'
-import Footer from './Footer/Footer'
-import HorizontalNav from './Navigation/HorizontalNav'
+import React from 'react';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import HorizontalNav from './Navigation/HorizontalNav';
+import { Container } from '@mui/material';
 
 const MainLayout = ({ children }) => {
   return (
     <div>
-      <Header />
-      <HorizontalNav />
-      {children}
-      <Footer />
+      <Container>
+        <Header />
+        <HorizontalNav />
+        {children}
+        <Footer />
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
