bf91344683326518cec14d32cb556f507096a9ec
Mi-Stap <mikeystaples@gmail.com>
2024-03-19 19:33:25 -0400

    resubmit email notification and user schema addition

--- capstone-team-1/server/server.js
+++ capstone-team-1/server/server.js
@@ -1,16 +1,19 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 const PORT = 4000;
 const app = express();
-import connectDB from './config/db.js';
-import colors from 'colors';
+import connectDB from "./config/db.js";
+import colors from "colors";
+import userRoutes from "./api/users.js";
 
 dotenv.config();
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`.yellow.bold);
--- capstone-team-1/src/components/User/UserList.jsx
+++ capstone-team-1/src/components/User/UserList.jsx
@@ -1,53 +1,67 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import axios from 'axios'
-import UserCard from './UserCard'
-import HorizontalNav from '../Navigation/HorizontalNav'
+import React, { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import axios from "axios";
+import UserCard from "./UserCard";
+import HorizontalNav from "../Navigation/HorizontalNav";
 
 const UserList = ({ handleEditClick }) => {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get('src/data/userList.json')
+  //       setUsers(response.data)
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error)
+  //     }
+  //   }
+
+  //   fetchData()
+  // }, [])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('src/data/userList.json')
-        setUsers(response.data)
+        // Update the URL to point to your Express server route
+        const response = await axios.get("/api/users");
+        setUsers(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error)
+        console.error("Error fetching data:", error);
       }
-    }
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
   return (
     <>
-      <div className='flex flex-col items-center mx-auto h-auto'>
-        <h2 className='text-2xl font-bold mb-4'>Users</h2>
-        <div className='flex justify-center items-center w-full py-2'>
-          <div className='flex-1 text-center'></div>
+      <div className="flex flex-col items-center mx-auto h-auto">
+        <h2 className="text-2xl font-bold mb-4">Users</h2>
+        <div className="flex justify-center items-center w-full py-2">
+          <div className="flex-1 text-center"></div>
           <button
             // onClick={handleCreateNewUser}
-            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-            style={{ position: 'absolute', right: '20px' }}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            style={{ position: "absolute", right: "20px" }}
           >
             Create New User
           </button>
         </div>
-        <div className='flex flex-col w-full px-4'>
+        <div className="flex flex-col w-full px-4">
           {users &&
             users.map((user) => (
               <UserCard
                 user={user}
-                key={user.id}
+                key={user._id}
                 handleEditClick={handleEditClick}
               />
             ))}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default UserList
+export default UserList;
