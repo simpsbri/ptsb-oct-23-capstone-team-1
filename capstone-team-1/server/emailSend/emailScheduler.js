2e0d59e7551f0574ba89847d94c27fdd10265ee5
simpsbri <simpsbri@gmail.com>
2024-03-26 06:15:03 -0500

    updates to emailScheduler to include 90 days past

--- capstone-team-1/server/emailSend/emailScheduler.js
+++ capstone-team-1/server/emailSend/emailScheduler.js
@@ -1,10 +1,12 @@
 import cron from 'node-cron'
 import sendEmailNotificationThirtyDays from './emailNotificationThirtyDays'
 import sendEmailNotificationSixtyDays from './emailNotificationSixtyDays'
+import sendEmailNotificationNinetyDays from './emailNotificationNinetyDays'
 
 // Schedule task to run every day at 7AM
 cron.schedule('0 7 * * *', function () {
   console.log('Running sendEmailNotification at 07:00 AM every day')
   sendEmailNotificationThirtyDays()
   sendEmailNotificationSixtyDays()
+  sendEmailNotificationNinetyDays()
 })
