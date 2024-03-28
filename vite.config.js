b9991be257b608d73ccad6bcc3c39948add2cefe
simpsbri <simpsbri@gmail.com>
2024-03-28 18:04:32 -0500

    Updates to BusinessCard, BusinessProfile, and HorizontalNav for css declaration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import resolve from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+})
