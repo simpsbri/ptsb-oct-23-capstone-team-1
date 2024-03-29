3e716b0e9608218f0a7d34a5eac47f93883887a1
Mi-Stap <mikeystaples@gmail.com>
2024-03-29 14:12:29 -0400

    commit before switching branches to pull updated in main

---
+++ src/routes/newProjectProfile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectProfileBlank from "../components/ProjectList/ProjectProfileBlank"; // Update the import path as needed
+
+const ProjectBlank = () => {
+  return (
+    <div>
+      <ProjectProfileBlank />
+    </div>
+  );
+};
+
+export default ProjectBlank;
