f71b9f1639700bdbd17030574f76f18cdfdceb71
simpsbri <simpsbri@gmail.com>
2024-03-30 23:03:37 -0500

    change in state of token

--- server/middleware/setAuth.jsx
+++ server/middleware/setAuth.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react'
 export const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState({ token: false })
+  const [auth, setAuth] = useState({ token: true })
 
   // your authentication logic...
 
