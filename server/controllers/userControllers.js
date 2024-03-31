0191cdabc07e59ee51e5bfb935deb4a3d6f52d37
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-03-31 18:43:10 -0500

    fixed route for userModel schema

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const generateToken = require('../config/generateToken');
-const User = require('../models/User');
+const User = require('../models/userModel');
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
