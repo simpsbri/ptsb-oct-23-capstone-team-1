b64caf36d365ab2caf2a60dbc818a81a8e877818
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-04-08 15:36:12 -0500

    added in hero image

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import '../../index.css'; //not sure if this is the right path
 import { Button } from '../Button/Button';
 import './HeroSection.css';
+import '../../output.css';
 
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-2.mp4" autoPlay loop muted />
       <h1>
         Technical Bootcamps + Work Gap Solution = Free Work Solutions for
         Businesses!
@@ -23,7 +23,7 @@ function HeroSection() {
         </Button>
         <Button
           className="btns"
-          buttonStyle="btn--primary"
+          buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
           LEARN MORE ABOUT BOOTCAMPS <i className="far fa-play-circle"></i>
