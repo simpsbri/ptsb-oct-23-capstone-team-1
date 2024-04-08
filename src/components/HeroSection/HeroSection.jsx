ee67ced38d9406f84895332105853c71345c84e3
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-04-08 15:04:02 -0500

    updated content to match work gap solution

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -7,7 +7,11 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/videos/video-2.mp4" autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
+      <h1>
+        Technical Bootcamps + Work Gap Solution = Free Work Solutions for
+        Businesses!
+      </h1>
+
       <p>What are you waiting for?</p>
       <div className="hero-btns">
         <Button
@@ -15,14 +19,14 @@ function HeroSection() {
           buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
-          GET STARTED
+          STUDENT INFORMATION
         </Button>
         <Button
           className="btns"
           buttonStyle="btn--primary"
           buttonSize="btn--large"
         >
-          Watch Trailer <i className="far fa-play-circle"></i>
+          LEARN MORE ABOUT BOOTCAMPS <i className="far fa-play-circle"></i>
         </Button>
       </div>
     </div>
