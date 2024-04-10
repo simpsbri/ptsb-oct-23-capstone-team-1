81ff48028f86e3d294e6962a76193b1875d58e09
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-04-10 18:15:47 -0500

    adding in logout function and resolved page refresh issue with storedAuth variable

--- server/middleware/setAuth.jsx
+++ server/middleware/setAuth.jsx
@@ -16,16 +16,24 @@ export const AuthProvider = ({ children }) => {
   const login = (token, isAdmin, user) => {
     setAuth({ token, isAdmin, user });
     setIsLoading(false);
+    // Save to local storage
+    localStorage.setItem('auth', JSON.stringify({ token, isAdmin, user }));
   };
 
-  // Simulate fetching auth state from an API
   useEffect(() => {
     const fetchAuthState = async () => {
       // Fetch auth state from API...
       // For now, we'll just simulate a delay
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      // await new Promise((resolve) => setTimeout(resolve, 2000))
 
-      login('fake-token', true, 'fake-user');
+      // Retrieve auth state from local storage
+      const storedAuth = localStorage.getItem('auth');
+      if (storedAuth) {
+        const { token, isAdmin, user } = JSON.parse(storedAuth);
+        login(token, isAdmin, user);
+      } else {
+        login('fake-token', true, 'fake-user');
+      }
     };
 
     fetchAuthState();
@@ -45,6 +53,20 @@ export const AuthProvider = ({ children }) => {
       </div>
     );
   }
+
+  const logout = () => {
+    // Clear the auth state
+    setAuth({
+      token: null,
+      isAdmin: null,
+      user: null,
+    });
+
+    // Clear the auth data from local storage
+    localStorage.removeItem('auth');
+  };
+
+  // Pass the logout function to the context provider
 
   return (
     <AuthContext.Provider value={{ auth, login }}>
