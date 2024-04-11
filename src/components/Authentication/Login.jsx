2ce75a39cd17a9d2a9fde0428979969b8d98d500
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-04-10 19:43:30 -0500

    no changes?

--- src/components/Authentication/Login.jsx
+++ src/components/Authentication/Login.jsx
@@ -1,103 +1,109 @@
-import React from 'react'
-import { Box, Snackbar, IconButton } from '@mui/material'
-import { Stack } from '@mui/material'
-import Button from '@mui/material/Button'
-import { useState } from 'react'
-import PasswordInput from './PasswordInput'
-import EmailInput from './EmailInput'
-import axios from 'axios'
-import { useContext } from 'react'
-import { AuthContext } from '../../../server/middleware/setAuth'
+import React from 'react';
+import { Box, Snackbar, IconButton } from '@mui/material';
+import { Stack } from '@mui/material';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import PasswordInput from './PasswordInput';
+import EmailInput from './EmailInput';
+import axios from 'axios';
+import { useContext } from 'react';
+import { AuthContext } from '../../../server/middleware/setAuth';
+import { useEffect } from 'react';
 
-const viteUrl = import.meta.env.VITE_WEB_ADDRESS
-import CloseIcon from '@mui/icons-material/Close'
-
-// // SNACKBAR CODE SNIP
-
-// ////////////////////////////////////////////////////////////////////////////////////////////////
+const viteUrl = import.meta.env.VITE_WEB_ADDRESS;
+import CloseIcon from '@mui/icons-material/Close';
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
-  // Snackbar state and handlers
-  const [open, setOpen] = useState(false)
-  const handleClick = () => setOpen(true)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const { login } = useContext(AuthContext);
+
+  useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+    if (userInfo && userInfo.token) {
+      login(userInfo.token, userInfo.role, userInfo);
+    }
+  }, []);
+
+  // // Snackbar state and handlers
+  const [open, setOpen] = useState(false);
+  const handleClick = () => setOpen(true);
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') return
-    setOpen(false)
-  }
-  const [snackbarMessage, setSnackbarMessage] = useState('')
+    if (reason === 'clickaway') return;
+    setOpen(false);
+  };
+  const [snackbarMessage, setSnackbarMessage] = useState('');
 
   const action = (
     <React.Fragment>
-      <Button color='secondary' size='small' onClick={handleClose}>
+      <Button color="secondary" size="small" onClick={handleClose}>
         UNDO
       </Button>
       <IconButton
-        size='small'
-        aria-label='close'
-        color='inherit'
+        size="small"
+        aria-label="close"
+        color="inherit"
         onClick={handleClose}
       >
-        <CloseIcon fontSize='small' />
+        <CloseIcon fontSize="small" />
       </IconButton>
     </React.Fragment>
-  )
+  );
 
-  // const { login } = useContext(AuthContext); // Destructure login from context
-  console.log(viteUrl)
-  const { login } = useContext(AuthContext)
+  console.log(viteUrl);
+  // const { login } = useContext(AuthContext);
   const submitHandler = async (e) => {
-    e.preventDefault()
-    setLoading(true)
+    e.preventDefault();
+    setLoading(true);
     if (!email || !password) {
-      console.error('Please fill in all fields')
-      setLoading(false)
-      return
+      console.error('Please fill in all fields');
+      setLoading(false);
+      return;
     }
     try {
       const config = {
         headers: {
           'Content-Type': 'application/json',
         },
-      }
+      };
 
       const { data } = await axios.post(
         `${viteUrl}api/user/login`,
         { email, password },
-        config,
-      )
-      login(data.token, data.role, data) // Pass the user's role as the second argument
-      console.log(data) // Log the response data to the console
+        config
+      );
+      login(data.token, data.role, data); // Pass the user's role as the second argument
+      console.log(data); // Log the response data to the console
 
       // login(data.token, data.user.role, data.user); // Update context with the user's information
 
-      localStorage.setItem('userInfo', JSON.stringify(data))
+      localStorage.setItem('userInfo', JSON.stringify(data));
       // Handle the response here
 
       // Clear the email and password fields
-      setEmail('')
-      setPassword('')
+      setEmail('');
+      setPassword('');
 
       // Scroll to the top of the page
-      window.scrollTo(0, 0)
+      window.scrollTo(0, 0);
 
-      setSnackbarMessage('Login successful') // Set the Snackbar's message
-      handleClick() // Open the Snackbar
+      setSnackbarMessage('Login successful'); // Set the Snackbar's message
+      handleClick(); // Open the Snackbar
     } catch (err) {
-      console.error(err.message)
-      setSnackbarMessage('Error login, try again') // Set the Snackbar's message
-      handleClick() // Open the Snackbar
+      console.error(err.message);
+      setSnackbarMessage('Error login, try again'); // Set the Snackbar's message
+      handleClick(); // Open the Snackbar
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   return (
-    <section className='p-10 bg-primary_dark_cyan flex w-full flex-row '>
+    <section className="p-10 bg-primary_dark_cyan flex w-full flex-row ">
       <div>
-        <h1 className='text-xl font-bold mb-4'>Login</h1>
+        <h1 className="text-xl font-bold mb-4">Login</h1>
         <form onSubmit={submitHandler}>
           <Box>
             <EmailInput
@@ -126,16 +132,16 @@ const Login = () => {
           </Box>
 
           <Box
-            component='div'
+            component="div"
             sx={{
               '& > :not(style)': { m: 1, width: '30ch' },
             }}
             noValidate
-            autoComplete='off'
-            className='p-2'
+            autoComplete="off"
+            className="p-2"
           >
-            <Stack spacing={2} direction='row'>
-              <Button variant='contained' type='submit'>
+            <Stack spacing={2} direction="row">
+              <Button variant="contained" type="submit">
                 Submit
               </Button>
             </Stack>
@@ -150,7 +156,7 @@ const Login = () => {
         action={action}
       />
     </section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
