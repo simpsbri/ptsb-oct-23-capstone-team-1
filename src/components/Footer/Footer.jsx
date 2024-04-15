aa2923cc01ecb505e0210b7779dce3f20445df57
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-04-15 08:31:14 -0500

    added in code for yearly updates in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
-    <div className='max-w-7xl mx-auto flex flex-col sm:flex-row p-20 '>
-      <h4 className='text-dark_gray_cyan mx-auto'>
-        ©2024 Work Gap Solution, SmartMomGig.com, & The Smart Gig Series
-        Intellectual Property. All rights reserved
+    <div className="max-w-7xl mx-auto flex flex-col sm:flex-row p-20 ">
+      <h4 className="text-dark_gray_cyan mx-auto">
+        © {new Date().getFullYear()} Work Gap Solution, SmartMomGig.com, & The
+        Smart Gig Series Intellectual Property. All rights reserved
       </h4>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
