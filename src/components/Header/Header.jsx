be337fa1b4135ad6898107acb27d638dd25e5738
Adan Navejas-Gallegos <adan.navejasgallegos@kochgs.com>
2024-04-16 08:45:22 -0500

    added in dark mode toggle component code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,41 @@
-import Background from './Background'
-import './Header.css'
-import { useLocation } from 'react-router-dom'
+import Background from './Background';
+import './Header.css';
+import { useLocation } from 'react-router-dom';
+import ToggleColorMode from '../Darkmode/Darkmode';
 
 const Header = () => {
-  const location = useLocation()
+  const location = useLocation();
 
   return (
-    <header className='h-28 w-full bg-primary_dark_cyan display-flex align-items-center'>
+    <header className="h-28 w-full bg-primary_dark_cyan display-flex align-items-center">
       <Background />
+      <ToggleColorMode />
       {location.pathname === '/' && (
-        <div className='header-links'>
+        <div className="header-links">
           <a
-            href='https://workgapsolution.com/faqs'
-            target='_blank'
-            className='header-link'
+            href="https://workgapsolution.com/faqs"
+            target="_blank"
+            className="header-link"
           >
             FAQs
           </a>
-          <a href='#link2' className='header-link'>
+          <a href="#link2" className="header-link">
             SmartGig Series
           </a>
-          <a href='#link3' className='header-link'>
+          <a href="#link3" className="header-link">
             Sign In
           </a>
           <a
-            href='https://calendly.com/workgapsolution/30min'
-            target='_blank'
-            className='form-button'
+            href="https://calendly.com/workgapsolution/30min"
+            target="_blank"
+            className="form-button"
           >
-            <button className='book-a-call-button'>Book a Call</button>
+            <button className="book-a-call-button">Book a Call</button>
           </a>
         </div>
       )}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
